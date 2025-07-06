4497faeb58a62dbe0a1e4210bf172aa7201d53d1
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-06 13:46:03 +0000

    updated my code

--- Ass3.py
+++ Ass3.py
@@ -5,6 +5,8 @@ x = 5
 y = 2.5
 
 
-print(f"The sum of {x} and {y} is {x + y} and the type of this variable is {type(x + y)}")
+print(f"The sum of {x} and {y} is {x + y}")
 
 print(f"The product of {x} and {y} is {x * y}")
+
+print(f"The type of this variable is {type(x + y)}")
