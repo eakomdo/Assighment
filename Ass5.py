1eae4721c4b04368f6017ebc55c63fafb1064af4
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-06 13:20:16 +0000

    updated my code

---
+++ Ass5.py
@@ -0,0 +1,10 @@
+#A program that asks a user to enter two numbers, and then performs basic arithmetic operations on them
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+print(f"The difference of {num1} and {num2} is {num2 - num1}")
+print(f"The product of {num1} and {num2} is {num1 * num2}")
+print(f"The quotient of {num1} and {num2} is {num1 / num2}")
+print(f"The modulus of {num1} and {num2} is {num1 % num2}")
