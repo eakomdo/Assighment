c12b1c11bf23326340163a496c76693df3ec0c8d
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-06 13:36:38 +0000

    updated my code

--- Ass6.py
+++ Ass6.py
@@ -1,1 +1,9 @@
 #A program that compares the age of two people
+
+Emmanuel = 30
+Ebenezer = 30
+
+if Emmanuel > Ebenezer:
+    print("Emmanuel is older than Ebenezer.")
+else:
+    print({Emmanuel == Ebenezer})
