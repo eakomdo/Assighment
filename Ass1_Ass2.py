d602391320987490e62cbda627e3c77e1eb65c82
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-06 13:40:28 +0000

    updated my code

--- Ass1_Ass2.py
+++ Ass1_Ass2.py
@@ -5,14 +5,13 @@ My_Height = float(1.63)
 Is_student = True
 
 #Assigngment 2
-type_of_variable = type(My_name)
-print("My name is", My_name, "and the type of this variable is", type_of_variable)
 
-type_of_variable = type(My_Age)
-print("My age is", My_Age, "and the type of this variable is", type_of_variable)  
+print("My name is", My_name, "and the type of this variable is", type(My_name))
 
-type_of_variable = type(My_Height)
-print("My height is", My_Height, "and the type of this variable is", type_of_variable)
+print("My age is", My_Age, "and the type of this variable is", type(My_Age))
 
-type_of_variable = type(Is_student)
-print("My student status is", Is_student, "and the type of this variable is", type_of_variable)
+print("My height is", My_Height, "and the type of this variable is", type(My_Height))
+
+print("My height is", My_Height, "and the type of this variable is", type(My_Height))
+
+print("My student status is", Is_student, "and the type of this variable is", type(Is_student))
